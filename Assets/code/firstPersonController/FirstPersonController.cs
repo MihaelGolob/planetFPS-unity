4f2b47587eff9043fc9bab70ee9a860598a56339
mihael.golob <golob.mihael@gmail.com>
2023-12-27 19:15:17 +0100

    Add camera rotation, gravity and jumping

--- Assets/code/firstPersonController/FirstPersonController.cs
+++ Assets/code/firstPersonController/FirstPersonController.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections.Generic;
 using System.Numerics;
 using UnityEngine;
 using Vector3 = UnityEngine.Vector3;
 using Matrix4x4 = UnityEngine.Matrix4x4;
 using Quaternion = UnityEngine.Quaternion;
+using Vector2 = UnityEngine.Vector2;
 
 public class FirstPersonController : MonoBehaviour {
     // inspector assigned
@@ -15,6 +17,8 @@ public class FirstPersonController : MonoBehaviour {
     [Header("Movement parameters")] 
     [SerializeField] private float moveSpeed;
     [SerializeField] private float airSpeed;
+    [SerializeField] private Vector2 upDownAngleRotation = new (-85f, 75f);
+    [SerializeField] private float jumpHeight = 5;
 
     [Header("Gravity")] 
     [SerializeField] private float gravityAcceleration = 9.8f;
@@ -26,10 +30,13 @@ public class FirstPersonController : MonoBehaviour {
     private Transform _cameraTransform;
 
     private bool _isGrounded;
+    private float _gravitySpeed;
     
     // key tracking
     private Dictionary<KeyCode, short> _keysDictionary = new();
     private List<KeyCode> _keysToTrack = new() {KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D, KeyCode.Space};
+    private Vector3 _lastMousePosition;
+    private float _cameraAngle;
 
     private void Start() {
         _rootTransform = rootObject.transform;
@@ -44,6 +51,16 @@ public class FirstPersonController : MonoBehaviour {
     }
 
     private void UpdateRotation() {
+        var mousePosition = Input.mousePosition;
+        var mouseDeltaPosition = mousePosition - _lastMousePosition;
+        _lastMousePosition = mousePosition;
+
+        // y axis
+        _cameraAngle += mouseDeltaPosition.y;
+        _cameraAngle = Mathf.Clamp(_cameraAngle, upDownAngleRotation.x, upDownAngleRotation.y);
+        _cameraTransform.localRotation = Quaternion.Euler(-_cameraAngle, 0, 0);
+        // x axis
+        _bodyTransform.localRotation *= Quaternion.Euler(0, mouseDeltaPosition.x, 0);
     }
 
     private void UpdateMovement() {
@@ -60,12 +77,28 @@ public class FirstPersonController : MonoBehaviour {
        // apply movement
        _rootTransform.position += moveVector;
        
-       // apply gravity
+       // apply gravitational rotation
        var gravityDir = (gravitySource.position - _rootTransform.position).normalized;
        var bodyDownDir = -_rootTransform.up.normalized;
        var gravityRotation = Quaternion.FromToRotation(bodyDownDir, gravityDir);
-       Debug.Log(gravityRotation.eulerAngles);
        _rootTransform.rotation *= gravityRotation;
+
+       _isGrounded = (gravitySource.position - _rootTransform.position).magnitude <= 10f; // TODO: check for isGrounded with colliders
+       
+       // apply gravity
+       if (_isGrounded && _gravitySpeed <= 0) {
+           _gravitySpeed = 0;
+
+           if (_keysDictionary[KeyCode.Space] > 0) {
+               _gravitySpeed = jumpHeight;
+           }
+       } else {
+           _gravitySpeed += -gravityAcceleration * Time.deltaTime;
+       }
+       
+       _isGrounded = false;
+       var gravityVector = gravityDir * (-_gravitySpeed * Time.deltaTime);
+       _rootTransform.position += gravityVector;
     }
 
     private void UpdateKeys() {
